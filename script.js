f4ce01733db967a82bf2f6b3dac9f8af0479065d
Olesia Polevoi <olesia.polevoi@gmail.com>
2021-12-12 13:42:26 -0500

    lesson 11 complete

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+"use strict";
 
+const btn = document.getElementById("btn");
 
+const changeBtnColor = function () {
+  const textInput = document.getElementById("text");
+  const input = textInput.value;
 
+  btn.style.backgroundColor = input;
+};
 
+btn.addEventListener("click", changeBtnColor);
 
+document.getElementById("e_btn").style.display = "none";
+
+const range = document.getElementById("range");
+
+const changeCircleSize = function (event) {
+  const span = document.getElementById("range-span");
+
+  const circle = document.getElementById("circle");
+  console.log(event);
+
+  span.textContent = event.target.value;
+
+  circle.style.width = event.target.value + "%";
+  circle.style.height = event.target.value + "%";
+};
+
+range.addEventListener("input", changeCircleSize);
